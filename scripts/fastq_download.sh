95edd14b0f365aad4e1c0c2cb408edaee9990d98
Firas Zemzem <131014717+Zemzemfiras1@users.noreply.github.com>
2024-02-07 16:01:38 +0100

    download fq files

---
+++ scripts/fastq_download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Directory for downladed reads
+mkdir -p Reads
+
+# Change directory to Scripts
+cd Scripts || exit 1
+
+# Read URLs from download.txt and download into Reads directory
+while read -r url; do
+    wget -c "$url" -P "../Reads"
+done < download.txt
+
