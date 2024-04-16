9615b517cecfbe8102dc2f1d8fe7f2422b7940e2
Kimberly Coetzer <78590097+Kimmiecc19@users.noreply.github.com>
2024-04-16 20:39:19 +0200

    Update filter_vcf.py

--- scripts/filter_vcf.py
+++ scripts/filter_vcf.py
@@ -24,35 +24,25 @@ def extract_info(vcf_file, output_file):
 
             # Extract CSQ information
             csq_info = [x.split('=')[1] for x in info_fields if x.startswith('CSQ=')]
-            csq_info = csq_info[0] if csq_info else '.'
+            csq_info = csq_info[0].split(',')[0] if csq_info else '.'
 
             # Extract CLNDN information
             clndn_info = [x.split('=')[1] for x in info_fields if x.startswith('CLNDN=')]
             clndn_info = clndn_info[0] if clndn_info else '.'
 
-            # Extract CLINSIG information
+            # Extract CLNSIG information
             clnsig_info = [x.split('=')[1] for x in info_fields if x.startswith('CLNSIG=')]
             clnsig_info = clnsig_info[0] if clnsig_info else '.'
 
-            # Write extracted information to output file
-            for csq_entry in csq_info.split(','):
-                csq_fields = csq_entry.split('|')
-                csq_dict = dict(zip([
-                    'Allele', 'Consequence', 'IMPACT', 'SYMBOL', 'Gene', 'Feature_type',
-                    'Feature', 'BIOTYPE', 'EXON', 'INTRON', 'HGVSc', 'HGVSp', 'cDNA_position',
-                    'CDS_position', 'Protein_position', 'Amino_acids', 'Codons',
-                    'Existing_variation', 'DISTANCE', 'STRAND', 'FLAGS', 'SYMBOL_SOURCE',
-                    'HGNC_ID'
-                ], csq_fields))
+            # Build INFO field with CSQ, CLNDN, and CLNSIG
+            info_field = f"CSQ={csq_info};CLNDN={clndn_info};CLNSIG={clnsig_info}"
 
-                # Write formatted CSQ fields along with other relevant columns
-                output.write(f"{chrom}\t{pos}\t{identifier}\t{ref}\t{alt}\t{qual}\t{filter_info}\t")
-                output.write(";".join([f"{heading}={csq_dict.get(heading, '.')}" for heading in csq_dict.keys()]))
-                output.write(f";CLNDN={clndn_info};CLNSG={clnsig_info}\t{format_col}\t{name_col}\n")
-
+            # Write formatted VCF line
+            output.write(f"{chrom}\t{pos}\t{identifier}\t{ref}\t{alt}\t{qual}\t{filter_info}\t"
+                         f"{info_field}\t{format_col}\t{name_col}\n")
 
 # Usage
 vcf_file = 'output_panel.vcf'
-output_file = 'filtered_vcf.vcf'
+output_file = 'output_filter.vcf'
 
 extract_info(vcf_file, output_file)
